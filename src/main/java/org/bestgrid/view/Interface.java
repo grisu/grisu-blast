000e643868275d2252bf86bbfeefde2ac574fd20
Johansen Sidabutar <pps24@uclive.ac.nz>
2011-05-24 14:21:04 +1200

    making changes with controller and data model

--- src/main/java/org/bestgrid/view/Interface.java
+++ src/main/java/org/bestgrid/view/Interface.java
@@ -1,16 +1,16 @@
 package org.bestgrid.view;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 import grisu.control.ServiceInterface;
 import grisu.control.exceptions.JobPropertiesException;
 import grisu.control.exceptions.JobSubmissionException;
 import grisu.frontend.control.login.LoginManager;
 import grisu.frontend.model.job.JobObject;
-import grisu.jcommons.constants.Constants;
+//import grisu.jcommons.constants.Constants;
 //import grisu.model.FileManager;
-import org.apache.commons.cli.*;
-import org.bestgrid.model.BlastJobCLI;
+//import org.apache.commons.cli.*;
+//import org.bestgrid.model.BlastJobCLI;
 
 import control.CLIController;
 
@@ -29,8 +29,37 @@ public class Interface {
 
 		System.out.println("Creating job...");
 		
-		CLIController control = new CLIController(args);
+		CLIController jobControl = new CLIController();
+		
+		jobControl.setServiceInterface(si);
+		jobControl.createJob();
+		jobControl.process(args);
 
+		JobObject theJob = jobControl.getJobObject().createJobObject();
+		
+		try {
+			System.out.println("Creating job on backend...");
+			//job.createJob("/ARCS/LocalAccounts/CanterburyHPC");
+			theJob.createJob();
+		} catch (JobPropertiesException e) {
+			System.err.println("Could not create job: "
+					+ e.getLocalizedMessage());
+			System.exit(1);
+		}
+
+		try {
+			System.out.println("Submitting job to the grid...");
+			//job.submitJob();
+			theJob.submitJob();
+		} catch (JobSubmissionException e) {
+			System.err.println("Could not submit job: "
+					+ e.getLocalizedMessage());
+			System.exit(1);
+		} catch (InterruptedException e) {
+			System.err.println("Jobsubmission interrupted: "
+					+ e.getLocalizedMessage());
+			System.exit(1);
+		}
 
 		// it's good practise to shutdown the jvm properly. There might be some
 		// executors running in the background
